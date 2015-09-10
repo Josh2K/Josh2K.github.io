41d1be4d09d0a573178a3a8e8d9fdf798306b462
Snapula <felkerjosh@gmail.com>
2015-09-09 20:09:00 -0400

    josh test

--- gitcommit.py
+++ gitcommit.py
@@ -13,16 +13,20 @@ import csv
 import time
 import datetime
 from subprocess import call
+import strava1
 
         
 def main():
     print 'Running parent script'
     time.sleep(10)
+    a = 'josh test'
     print 'GIT commiting...'
-    call(["git", "commit","-a","-m", "autorun"])
+    call(["git", "commit","-a","-m", a])
     time.sleep(10)
     print 'GIT pushing...'
     call(["git", "push"])
     
+
+
 if __name__ == "__main__":
   main()
