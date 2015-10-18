6adf62644cd40e161097039785bbd0ffa342bebe
Snapula <felkerjosh@gmail.com>
2015-10-18 00:23:25 -0400

    autorun - 2015-10-18 00:23:23.587000

--- strava1.py
+++ strava1.py
@@ -12,10 +12,10 @@ import csv
 import time
 import datetime
 import json
+import operator
 
 
-
-def trim_count():
+def trim_count_overtime():
     friend_colour_dict = {}
     friend_colour_file = open('friend_colour.csv')
     colourreader = csv.DictReader(friend_colour_file)
@@ -51,7 +51,7 @@ def trim_count():
                     writer.writerow(linelist)
     return
 
-def json_convert():
+def json_convert_trim_count_overtime():
     infile = open('trimmed_segmentcountovertime.csv')
     reader = csv.reader(infile)
     d_list = defaultdict(list)
@@ -107,6 +107,41 @@ def json_convert():
     outfile.close()
     return
 
+def json_convert_segmentcount():
+    friend_colour_dict = {}
+    friend_count_dict = {}
+
+    friend_count_file = open('segmentcount.csv')
+    countreader = csv.DictReader(friend_count_file)
+    for line in countreader:
+        friend_count_dict[line["name"]] = int(line["count"])
+        friend_colour_dict[line["name"]] = '#'+line["colour"]
+
+
+    listofdicts =[]
+    for name,count in sorted(friend_count_dict.items(), key=operator.itemgetter(1), reverse=True):
+        newdict = {}
+        newdict['label'] = name
+        newdict['value'] = friend_count_dict[name]
+        newdict['color'] = friend_colour_dict[name]  
+        listofdicts.append(newdict)
+           
+    outfile = open('segmentcount.json', 'w')
+    outfile.write('['+'\n')
+    outfile.close()
+
+    singlenamedic = {}
+    singlenamedic["key"] = 'Segment Count'
+    singlenamedic["values"] = listofdicts
+    with open('segmentcount.json', 'a+') as outfile:
+        json.dump(singlenamedic, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
+        outfile.write('\n')
+
+    outfile = open('segmentcount.json', 'a+')
+    outfile.write('\n'+']')
+    outfile.close()
+    return
+
 def getSec(s):
     l = s.split(':')
     return int(l[0]) * 3600 + int(l[1]) * 60 + int(l[2])
@@ -127,7 +162,6 @@ def segment_details(num,segment,topguy,friend_colour_dict):
 
     tuple=(str(num),str(start_latitude),str(start_longitude),str(segment_name)+':  ['+str(topguy)+']',str(topguy),str(friend_colour_dict[topguy]),str(segment_name),str(segment_id),str(url))
     now = datetime.datetime.now().strftime('%Y-%m-%d')
-    print '\r'+'                                                                             ',
     print '\r'+str(now)+': ID: '+str(id)+'     Segment ID:  '+str(segment_id)+'   Owner:  '+str(topguy),
     return tuple
     
@@ -159,9 +193,7 @@ def main():
     athlete = client.get_athlete()
     print("Hello, {}. I know your email is {}".format(athlete.firstname, athlete.email))
     josh_friends = client.get_athlete_friends(5991862)
-    for a in josh_friends:
-        print("{} is Josh's friend.".format(a.firstname))
-        
+            
     #colors
     colours = ['575757','FFCDF3','FFEE33','FF9233','29D0D0','8126C0','814A19','1D6914','2A4BD7','AD2323','000000','88C6ED','82C341']
     
@@ -210,6 +242,8 @@ def main():
             badoutfile.close()
             pass
 
+
+    #segment count output
     segcountoutfile = open('segmentcount.csv', 'w')
     segcountoutfile.write('name,colour,count'+'\n')
     for x in friend_count_dict:
@@ -218,18 +252,19 @@ def main():
             segcountoutfile.write(str(x)+','+str(friend_colour_dict[x])+','+str(friend_count_dict[x])+'\n')
     segcountoutfile.write('\n')
     segcountoutfile.close()
+    json_convert_segmentcount()
+    
 
 
+    #segment count over time output
     segcountovertimefile = open('segmentcountovertime.csv', 'a+')
     nowdate = datetime.datetime.now().strftime('%Y-%m-%d')
     for x in friend_count_dict:
         if x != 'UNCLAIMED':
-            print str(x)+': '+str(friend_count_dict[x])
             segcountovertimefile.write(str(nowdate)+','+str(x)+','+str(friend_colour_dict[x])+','+str(friend_count_dict[x])+'\n')
     segcountovertimefile.close()
-
-    trim_count()
-    json_convert()
+    trim_count_overtime()
+    json_convert_trim_count_overtime()
               
 
 if __name__ == "__main__":
