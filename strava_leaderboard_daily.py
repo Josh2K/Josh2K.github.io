f367bdb085576c19f59a56ba3ad0033bd28902a4
Snapula <felkerjosh@gmail.com>
2016-03-22 19:03:30 -0400

    autorun - 2016-03-22 19:03:22.444000

--- strava_leaderboard_daily.py
+++ strava_leaderboard_daily.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python
 
 import sys
-import re
 import os
 import shutil
 import commands
-import csv
-from bs4 import BeautifulSoup
-import json
-import requests
-import csv
 import time
 import datetime
 import strava_leaderboard
