f9d897ad621f4e3aa98eb62e04e3b9f82502a738
Ubuntu <felkerjosh@gmail.com>
2017-09-29 01:43:10 +0000

    added strava_plots.py

--- strava_update.sh
+++ strava_update.sh
@@ -3,7 +3,8 @@ cd ~/Josh2K.github.io/
 git fetch origin
 git reset --hard origin/master
 python strava_leaderboard.py >> strava_leaderboard_py_log.txt 2>&1
-python strava1.py >> strava1_py_log.txt 2>&1
+python strava_segments.py >> strava_segments_py_log.txt 2>&1
+python strava_plots.py >> strava_plots.py_log_txt 2>&1
 git add .
 git commit -a -m 'cron attempt'
 git push
